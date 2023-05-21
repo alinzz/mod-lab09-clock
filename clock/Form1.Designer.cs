6e1d60d794ddffbd64837470474e3892b7f448cf
alinzz <97335671+alinzz@users.noreply.github.com>
2023-05-21 21:53:54 +0300

    new

---
+++ clock/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace mod_lab09_clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Name = "Form1";
+            this.Text = "analog clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Timers.Timer timer1;
+
+        #endregion
+    }
+}
---
+++ clock/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace mod_lab09_clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            GraphicsState gs;
+
+            int w = Width;
+            int h = Height;
+
+            DateTime dt = DateTime.Now;
+
+            g.TranslateTransform(w / 2, h / 2);
+
+            Pen clock_pen = new Pen(Color.White, 3);
+            Pen p1 = new Pen(Color.Black, 4);
+            Pen p2 = new Pen(Color.Blue, 4);
+            Pen p3 = new Pen(Color.Indigo, 4);
+            Brush b1 = new SolidBrush(Color.Indigo);
+            Brush b2 = new SolidBrush(Color.Black);
+
+            g.DrawEllipse(clock_pen, -120, -120, 240, 240);
+
+
+            for (int i = 0; i < 12; i++)
+            {
+                gs = g.Save();
+                g.RotateTransform(30 * i + 45);
+                g.DrawLine(clock_pen, -70, -70, -85, -85);
+                g.Restore(gs);
+            }
+
+            for (int i = 0; i < 60; i++)
+            {
+                gs = g.Save();
+                g.RotateTransform(6 * i + 45);
+                g.DrawLine(clock_pen, -80, -80, -85, -85);
+                g.Restore(gs);
+            }
+
+            gs = g.Save();
+            g.RotateTransform(6 * (dt.Hour + (float)dt.Minute / 60));
+            g.DrawLine(p3, 0, 0, 0, -40);
+            Point point1 = new Point(10, -40);
+            Point point2 = new Point(-10, -40);
+            Point point3 = new Point(0, -60);
+            Point[] points1 = { point1, point2, point3 };
+            g.FillPolygon(b1, points1);
+            g.Restore(gs);
+
+            gs = g.Save();
+            g.RotateTransform(6 * dt.Second);
+            g.DrawLine(p2, 0, 0, 0, -110);
+            g.Restore(gs);
+
+            gs = g.Save();
+            g.RotateTransform(6 * (dt.Minute + (float)dt.Second / 60));
+            g.DrawLine(p1, 0, 0, 0, -50);
+            Point point4 = new Point(5, -50);
+            Point point5 = new Point(-5, -50);
+            Point point6 = new Point(0, -80);
+            PointF[] points2 = { point4, point5, point6 };
+            g.FillPolygon(b2, points2);
+            g.Restore(gs);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            this.Invalidate();
+        } 
+    }
+}
